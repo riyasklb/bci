a3e740608caddb5ff0aa4c37c5821a0be8cb6ea9
sharmi4 <shapoqueen2025@gmail.com>
2023-04-29 13:01:40 +0530

    hii world

--- lib/authentications/langing_screen/landing_screen.dart
+++ lib/authentications/langing_screen/landing_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bci/authentications/generate_otp/generate_otp.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -6,6 +7,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
 import '../../constands/constands.dart';
+import '../../views/business/home_screen.dart';
 
 class landing_screen extends StatelessWidget {
   const landing_screen({super.key});
@@ -16,62 +18,72 @@ class landing_screen extends StatelessWidget {
     return Container(
       height: size.height,
       width: size.width,
-
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('assets/images/01 Login Screen Options.png',),fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/images/01 Login Screen Options.png',
+          ),
+          fit: BoxFit.cover,
         ),
       ),
-      child: Scaffold(body:   Column(mainAxisAlignment: MainAxisAlignment.end,
-        children: [Image.asset('assets/images/Group 5814.png'),
-          SizedBox(
-                        height: 65,
-                        width: double.infinity,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color.fromARGB(255, 177, 126, 60),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(4.0),
-                                ),
-                              ),
-                              onPressed: () {
-                             Get.off(Generate_otp());
-                              },
-                              child: Text(
-                                'Member Login',
-                                style: TextStyle(fontSize: 22,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              )),
+      child: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset('assets/images/Group 5814.png'),
+              SizedBox(
+                height: 65,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 177, 126, 60),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
                         ),
-                      ), ksizedbox20,    SizedBox(
-                        height: 65,
-                        width: double.infinity,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor:Color.fromARGB(0, 246, 246, 246),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(4.0),
-                                ),
-                              ),
-                              onPressed: () {
-                              
-                              },
-                              child: Text(
-                                'Bussiness login',
-                                style: TextStyle(fontSize: 22,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              )),
+                      ),
+                      onPressed: () {
+                        Get.off(Generate_otp());
+                      },
+                      child: Text(
+                        'Member Login',
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      )),
+                ),
+              ),
+              ksizedbox20,
+              SizedBox(
+                height: 65,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(0, 246, 246, 246),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0),
                         ),
-                      ),ksizedbox30,
-        ],
-      ),  
-        backgroundColor: Colors.transparent),
+                      ),
+                      onPressed: () {
+                        Get.to(HomeScreen());
+                      },
+                      child: Text(
+                        'Bussiness login',
+                        style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      )),
+                ),
+              ),
+              ksizedbox30
+            ],
+          ),
+          backgroundColor: Colors.transparent),
     );
   }
 }
--- lib/constands/constands.dart
+++ lib/constands/constands.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 const SizedBox ksizedbox20 = SizedBox(height: 20);
 const SizedBox ksizedbox10 = SizedBox(height: 10);
 const SizedBox ksizedbox30 = SizedBox(height: 30);
@@ -7,17 +8,13 @@ const SizedBox ksizedbox40 = SizedBox(height: 40);
 const SizedBox kwidth10 = SizedBox(width: 10);
 const SizedBox kwidth5 = SizedBox(width: 5);
 
-
-
-
-
-
 Color kgrey = Colors.grey;
 Color kblue = Color(0xFF003366);
 Color kwhite = Colors.white;
 
 Color korange = Color(0xFFFF5003);
 
+Color kOrange = Color(0xFFff5003);
 
 final kpeople = Image.asset('assets/images/profile.svg');
 //final kimgadd = SvgPicture.asset('assets/images/add.svg');
